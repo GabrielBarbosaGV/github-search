322365ddf4d3ce3750ed3d8ed2fa0beb52c4d8b4
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2022-03-21 18:49:33 +0000

    Correcting command line argument to pdet_all.sh

--- scripts/pdet_all.sh
+++ scripts/pdet_all.sh
@@ -3,5 +3,5 @@
 for FOLDER_NAME in $(echo ./collected-projects/*/); do
 	TEXT_DESTINATION="$(echo $FOLDER_NAME | sed 's/\/$//g')".txt
 
-	[ -f $TEXT_DESTINATION ] || pdet $FOLDER_NAME --debug --crash --showDialog > $TEXT_DESTINATION 2>>errors.txt
+	[ -f $TEXT_DESTINATION ] || pdet $FOLDER_NAME --debug --crash --showRatio > $TEXT_DESTINATION 2>>errors.txt
 done
